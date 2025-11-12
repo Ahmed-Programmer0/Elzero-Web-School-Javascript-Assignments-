e506adfcb7f86ae149704990b226267063278459
Ahmed-Programmer0 <ahmedwaelforcollage@gmail.com>
2025-11-12 06:03:21 +0200

    Assigment 15 has been solved with 13 different ways for getting 100000

---
+++ Assigment_15/main.js
@@ -0,0 +1,40 @@
+// Printing 100000 with 13 different ways
+
+// Solution one
+console.log(1_00_000);
+
+// Solution Two
+console.log(10e4);
+
+// Solution Three
+console.log(10 ** 5);
+
+// Solution Four
+console.log(Math.pow(10, 5));
+
+// Solution Five
+console.log(100000.0);
+
+// Solution Six
+console.log(6e4 + 4e4);
+
+// Solution Seven
+console.log(1e4 * 10);
+
+// Solution Eight
+console.log(Number.parseInt("100000"));
+
+// Solution Nine
+console.log(1_000_00);
+
+// Solution Ten
+console.log(Math.ceil(99999.9999));
+
+// Solution Eleven
+console.log(Math.floor(100000.99999));
+
+// Solution Twelve
+console.log(Math.round(99999.9999));
+
+// Solution Thirteen
+console.log(Math.trunc(100000.233454));
