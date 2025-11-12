1b0e0b5c390680aa5341398c7fade68b5d491c95
Ahmed-Programmer0 <ahmedwaelforcollage@gmail.com>
2025-11-12 07:02:54 +0200

    Assigment 20 has been solved By Getting 10 by 5 different ways

---
+++ Assigment_20/main.js
@@ -0,0 +1,17 @@
+// Getting 10 by Five different Ways
+let flt = 10.4;
+
+// Solution One
+console.log(Math.trunc(flt));
+
+// Solution Two
+console.log(Math.floor(flt));
+
+// Solution Three
+console.log(Math.round(flt));
+
+// Solution Four
+console.log(Number(flt.toFixed(0)));
+
+// Solution Five
+console.log(Number.parseInt(flt));
