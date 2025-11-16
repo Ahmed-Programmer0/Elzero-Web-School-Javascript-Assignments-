68ede5bed379456572cd421408cb5a67f8bc4676
Ahmed-Programmer0 <ahmedwaelforcollage@gmail.com>
2025-11-17 01:22:15 +0200

    Assigment 24 has been solved, It Is About Using Operators To Get True In Each Statement

---
+++ Assigment_24/main.js
@@ -0,0 +1,25 @@
+// Making All Statements To Be True
+
+// First Statement
+console.log(100 == "100"); //True
+
+// Second Statement
+console.log(100 !== 1000); //True
+
+// Third Statement
+console.log(110 > 100 && 10 < 20); //True
+
+// Fourth Statement
+console.log(-10 == "-10"); //True
+
+// Fifth Statement
+console.log(!-50 === !+"-40"); //True
+
+// Sixth Statement
+console.log(!10 === !-"-40"); //True
+
+// Seventh Statement
+console.log(!"10" === !10); //True
+
+// Eight Statement
+console.log(!20 === false); //True
