b135e7362a4ff7bea287f11f2039a022858ad32b
Ahmed-Programmer0 <ahmedwaelforcollage@gmail.com>
2025-11-18 15:53:52 +0200

    Assigment 25 has been solved , It Is about Getting True For Six Times From Two Variables By Using Operators Which I have Learned

---
+++ Assigment_25/main.js
@@ -0,0 +1,22 @@
+// Getting True  at Least 6 Times From Two Variables
+// The Two Variables
+let num1 = 10;
+let num2 = 20;
+
+// First Time
+console.log(!num1 === !num2); //True
+
+// Second Time
+console.log(num1 !== num2); //True
+
+// Third Time
+console.log(num1 != num2); //True
+
+// Fourth Time
+console.log(num1 < num2); //True
+
+// Fifth Time
+console.log(num2 >= num1); //True
+
+// Sixth Time
+console.log(num2 > num1); //True
