b511c355c1197396b4f78196d58e9ea119266922
Ahmed-Programmer0 <ahmedwaelforcollage@gmail.com>
2025-11-22 18:06:28 +0200

    Assigment 27 has been solved , It is About Using If Conditions In Three Cases To Put Zeros Or Not In Front Of Numbers

---
+++ Assigment_27/main.js
@@ -0,0 +1,16 @@
+// This Assigment Is For Printing 0 Or 00 or nothing in Front Of Number
+// First Case 00 In front Of Number
+// let num = 9;
+// if (num < 10) {
+//   console.log(`00${num}`);
+// }
+// Second Case 0 In Front Of Number
+// let num = 20;
+// if (num > 10 && num < 100) {
+//   console.log(`0${num}`);
+// }
+// Third Case No Zeros In Front Of Number
+let num = 110;
+if (num >= 100) {
+  console.log(`${num}`);
+}
