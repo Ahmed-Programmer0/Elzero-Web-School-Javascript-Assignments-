0d7c643f99d1a9320538a3d8f4d5c891f0f67034
Ahmed-Programmer0 <ahmedwaelforcollage@gmail.com>
2025-11-24 10:35:03 +0200

    Assigment 30 has been solved , It Is A Good Assigment To Practice On Making Variable In 7 Condition Equal True

---
+++ Assigment_30/main.js
@@ -0,0 +1,79 @@
+// This Assigment Is About Editing Four Variables To Get 7 Times True
+// The main Variables Are Three Integers And One String It's Value is Number
+
+// Edit What You Want Here
+
+// The Main Values Of All Variables
+// let num1 = 10;
+// let num2 = 10;
+// let num3 = "10";
+// let num4 = 20;
+
+// The New Values That Make All 7 Conditions True
+
+let num1 = 4;
+let num2 = 3;
+let num3 = 4;
+let num4 = 26;
+
+/*
+    Do Not Edit Below This Line 
+    Needed Output
+    True 7 Times
+*/
+
+// Condition 1
+
+if (num1 > num2) {
+  console.log("True");
+} else {
+  console.log("False");
+}
+
+// Condition 2
+
+if (num1 > num2 && num1 < num4) {
+  console.log("True");
+} else {
+  console.log("False");
+}
+
+// Condition 3
+
+if (num1 > num2 && num1 === num3) {
+  console.log("True");
+} else {
+  console.log("False");
+}
+
+// Condition 4
+
+if (num1 + num2 < num4) {
+  console.log("True");
+} else {
+  console.log("False");
+}
+
+// Condition 5
+
+if (num1 + num3 < num4) {
+  console.log("True");
+} else {
+  console.log("False");
+}
+
+// Condition 6
+
+if (num1 + num2 + num3 < num4) {
+  console.log("True");
+} else {
+  console.log("False");
+}
+
+// Condition 7
+
+if (num4 - (num1 + num3) + num2 === 21) {
+  console.log("True");
+} else {
+  console.log("False");
+}
